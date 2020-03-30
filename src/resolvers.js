32a2d8d2ed968806f124eb621e17d4bb10b4a2b9
Kavan Samra <kssamra@ucsc.edu>
2020-03-30 15:58:33 -0700

    simple offset based pagination support

--- src/resolvers.js
+++ src/resolvers.js
@@ -2,6 +2,15 @@ module.exports = {
   Query: {
     hello: () => "Hello World",
     pokemon: (_, {id}, {dataSources}) => dataSources.pokemonAPI.getPokemonById({id}),
-    pokemons: (_,__,{dataSources}) => dataSources.pokemonAPI.getAllPokemon()
+    pokemons: (_,{pageSize=20, offset},{dataSources}) => {
+      console.log(`received request with pageSize=${pageSize} and offset=${offset}`);
+      pageSize = (offset + pageSize) > 801 ? 801 - offset : pageSize;
+      const pokemons = dataSources.pokemonAPI.getAllPokemon(pageSize, offset);
+      return {
+        pokemons, 
+        offset: offset + pageSize,
+        hasMore: offset < 801
+      }
+    }
   }
 };
